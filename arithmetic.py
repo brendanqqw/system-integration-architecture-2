c2b20d044f51f55e3372183d4035c181571b8715
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2023-11-20 14:39:58 +0800

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -6,7 +6,14 @@ difference = num1 - num2
 quotient = num1 / num2
 product = num1 * num2
 
+#ADDITION
 print("The sums is",sum)
-print("The differenceis",difference)
+
+#SUBTRACTION
+print("The difference is",difference)
+
+#DIVIDE
 print("The quotient is",quotient)
+
+#MULTIPLICATION
 print("The product is",product)
