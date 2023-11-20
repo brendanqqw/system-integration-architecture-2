9878eaa6da8b6d627b55675171017d1f7666051c
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2023-11-20 14:38:37 +0800

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+num1 = 20
+num2 = 30
+
+sum = num1 + num2
+difference = num1 - num2
+quotient = num1 / num2
+product = num1 * num2
+
+print("The sums is",sum)
+print("The differenceis",difference)
+print("The quotient is",quotient)
+print("The product is",product)
