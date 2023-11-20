14ec796e391faed5013819f6a1034d66eec3b2f3
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2023-11-20 15:18:27 +0800

    Create for-loops.py

---
+++ for-loops.py
@@ -0,0 +1,16 @@
+#Exercise 4 For Loops
+#Create a python program that displays all even numbers from 1 to 100 using for loops
+
+#EVEN
+for i in range(1, 100):
+    if i % 2 == 0:
+        print(i)
+
+#ODD
+for i in range(1, 100):
+  if i % 2 != 0:
+      print(i)
+
+#CONDITION
+#== even 
+#!= odd
