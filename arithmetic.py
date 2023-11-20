cef37fb508da7a95f004da932f3221116186e81e
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2023-11-20 14:46:40 +0800

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,19 +1,21 @@
+#VARIABLES
 num1 = 20
 num2 = 30
 
+#ARITHMETIC
 sum = num1 + num2
 difference = num1 - num2
 quotient = num1 / num2
 product = num1 * num2
 
 #ADDITION
-print("The sums is",sum)
+print("The sum of", num1 ,"and" ,num2,sum)
 
 #SUBTRACTION
-print("The difference is",difference)
+print("The difference of", num1, "and", num2,difference)
 
 #DIVIDE
-print("The quotient is",quotient)
+print("The quotient of", num1, "and", num2,quotient)
 
 #MULTIPLICATION
-print("The product is",product)
+print("The product of", num1, "and", num2,product)
